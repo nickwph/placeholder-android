3d6e02968cd7e087b6e3a7bf61ce86cc63417f00
Nicholas Wong <nickwph@gmail.com>
2018-08-08 23:46:13 +0800

    Set up all fragments.

---
+++ app/src/main/java/com/nicholasworkshop/placeholder/fragment/PhotoFragment.kt
@@ -0,0 +1,20 @@
+package com.nicholasworkshop.placeholder.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nicholasworkshop.placeholder.R
+
+
+class PhotoFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_photo, container, false)
+    }
+}
---
+++ app/src/main/java/com/nicholasworkshop/placeholder/fragment/TabFragment.kt
@@ -0,0 +1,44 @@
+package com.nicholasworkshop.placeholder.fragment
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import com.nicholasworkshop.placeholder.R
+import kotlinx.android.synthetic.main.fragment_tab.*
+
+
+class TabFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bottomNavigationView.setOnNavigationItemSelectedListener(NavigationItemSelectedListener())
+//        fragmentManager?.beginTransaction()
+//                .add()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_tab, container, false)
+    }
+
+    class NavigationItemSelectedListener : BottomNavigationView.OnNavigationItemSelectedListener {
+        override fun onNavigationItemSelected(item: MenuItem): Boolean {
+            when (item.itemId) {
+                R.id.navigation_home -> {
+
+                    return true
+                }
+                R.id.navigation_dashboard -> {
+                    return true
+                }
+                R.id.navigation_notifications -> {
+                    return true
+                }
+            }
+            return false
+        }
+    }
+}
